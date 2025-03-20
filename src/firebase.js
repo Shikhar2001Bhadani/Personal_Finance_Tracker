a327541be779f36150f7c743991926745d583877
Shikhar2001Bhadani <shkrbh30052001@gmail.com>
2025-03-20 21:08:33 +0530

    Second Commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth,GoogleAuthProvider} from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDx77QWiFjSbZUuI7-wUfMRbGL_ts0IwsE",
-  authDomain: "finance-tracker-80fed.firebaseapp.com",
-  projectId: "finance-tracker-80fed",
-  storageBucket: "finance-tracker-80fed.firebasestorage.app",
-  messagingSenderId: "618808178276",
-  appId: "1:618808178276:web:53f4c7cf61e3d532081171",
-  measurementId: "G-ZWCF5N28NP"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -18,4 +18,5 @@ const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
-export {db,auth,provider,doc,setDoc};
+
+export { db, auth, provider, doc, setDoc };
